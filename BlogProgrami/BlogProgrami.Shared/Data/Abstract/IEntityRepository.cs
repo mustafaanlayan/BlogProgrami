b43d3156b890f9e42e21a8e45741664f02765d6a
mustafaanlayan <32699486+mustafaanlayan@users.noreply.github.com>
2021-11-30 16:15:37 +0300

    commit2

---
+++ BlogProgrami/BlogProgrami.Shared/Data/Abstract/IEntityRepository.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+using BlogProgrami.Shared.Entities.Abstract;
+
+namespace BlogProgrami.Shared.Data.Abstract
+{
+   public interface IEntityRepository<T> where T:class,IEntity,new()
+   {
+       Task<T> GetAsync(Expression<Func<T,bool>>predicate,params Expression<Func<T,object>>[]inculeProperties);
+
+       Task<IList<T>> GetAllAsycn(Expression<Func<T, bool>> predicate = null,
+           params Expression<Func<T, object>>[] inculeProperties);
+
+       Task AddSycn(T entity);
+       Task UpdateAsycn(T entity);
+       Task DeleteAsycn(T entity);
+       Task<bool> AnnyAsycn(Expression<Func<T, bool>> predicate);
+       Task<int> CountAsycn(Expression<Func<T, bool>> predicate);
+
+   }
+}
