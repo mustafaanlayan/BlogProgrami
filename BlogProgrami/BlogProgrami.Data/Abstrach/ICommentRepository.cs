d36107915694e00a811e781256eec54d39d48c3f
mustafaanlayan <32699486+mustafaanlayan@users.noreply.github.com>
2021-12-01 22:35:06 +0300

    Commit Repository yapımı

---
+++ BlogProgrami/BlogProgrami.Data/Abstrach/ICommentRepository.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BlodProgrami.Entity.Concrete;
+using BlogProgrami.Shared.Data.Abstract;
+
+namespace BlogProgrami.Data.Abstrach
+{
+   public interface ICommentRepository:IEntityRepository<Comment>
+    {
+    }
+}
