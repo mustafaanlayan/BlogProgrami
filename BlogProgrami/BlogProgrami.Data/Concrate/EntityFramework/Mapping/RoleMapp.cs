48f8e2afbbb1b5d3a55235a91558d9bf06e3d561
mustafaanlayan <32699486+mustafaanlayan@users.noreply.github.com>
2021-12-04 16:20:42 +0300

    commit rolemapping

---
+++ BlogProgrami/BlogProgrami.Data/Concrate/EntityFramework/Mapping/RoleMapp.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BlodProgrami.Entity.Concrete;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace BlogProgrami.Data.Concrate.EntityFramework.Mapping
+{
+   public class RoleMapp:IEntityTypeConfiguration<Role>
+    {
+        public void Configure(EntityTypeBuilder<Role> builder)
+        {
+            builder.HasKey(r => r.Id);
+            builder.Property(r => r.Id).ValueGeneratedOnAdd();
+            builder.Property(r => r.Adi).IsRequired();
+            builder.Property(r => r.Adi).HasMaxLength(30);
+            builder.Property(r => r.Aciklama).IsRequired();
+            builder.Property(r => r.Aciklama).HasMaxLength(250);
+            builder.Property(r => r.CreatedByName).IsRequired();
+            builder.Property(r => r.CreatedByName).HasMaxLength(50);
+            builder.Property(r => r.ModifedByName).IsRequired();
+            builder.Property(r => r.ModifedByName).HasMaxLength(50);
+            builder.Property(r => r.CreatedDate).IsRequired();
+            builder.Property(r => r.ModifeDateTime).IsRequired();
+            builder.Property(r => r.IsActive).IsRequired();
+            builder.Property(r => r.IsDeleted).IsRequired();
+            builder.Property(r => r.Notlar).HasMaxLength(500);
+            builder.ToTable("Roller");
+
+        }
+    }
+}
