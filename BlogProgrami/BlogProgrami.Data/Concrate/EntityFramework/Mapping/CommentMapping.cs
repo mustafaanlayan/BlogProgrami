6398566afee75d9ca6c6baf412a1ae9dbcac41ef
mustafaanlayan <32699486+mustafaanlayan@users.noreply.github.com>
2021-12-04 16:14:20 +0300

    commit  commentmap

---
+++ BlogProgrami/BlogProgrami.Data/Concrate/EntityFramework/Mapping/CommentMapping.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BlodProgrami.Entity.Concrete;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace BlogProgrami.Data.Concrate.EntityFramework.Mapping
+{
+   public class CommentMapping:IEntityTypeConfiguration<Comment>
+    {
+        public void Configure(EntityTypeBuilder<Comment> builder)
+        {
+            builder.HasKey(c => c.Id);
+            builder.Property(c => c.Id).ValueGeneratedOnAdd();
+            builder.Property(c => c.Yazi).IsRequired();
+            builder.Property(c => c.Yazi).HasMaxLength(3000);
+            builder.HasOne<Article>(c => c.Article).WithMany(a=>a.Comments).HasForeignKey(c=>c.ArticleId);
+            builder.Property(c => c.CreatedByName).IsRequired();
+            builder.Property(c => c.CreatedByName).HasMaxLength(50);
+            builder.Property(c => c.ModifedByName).IsRequired();
+            builder.Property(c => c.ModifedByName).HasMaxLength(50);
+            builder.Property(c => c.CreatedDate).IsRequired();
+            builder.Property(c => c.ModifeDateTime).IsRequired();
+            builder.Property(c => c.IsActive).IsRequired();
+            builder.Property(c => c.IsDeleted).IsRequired();
+            builder.Property(c => c.Notlar).HasMaxLength(500);
+            builder.ToTable("Yorumlar");
+
+        }
+    }
+}
